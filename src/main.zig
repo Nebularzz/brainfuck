300f5ca9e5c386b3f98c0edc3a95f160c9c09269
Nebularzz <103582504+Nebularzz@users.noreply.github.com>
2025-12-13 11:49:13 +0100

    add reading bf source from stdin which allows piping

--- src/main.zig
+++ src/main.zig
@@ -2,37 +2,42 @@ const std = @import("std");
 const ir = @import("compiler/ir.zig");
 const codegen = @import("compiler/codegen.zig");
 
-var stdout_buf: [4096]u8 = undefined;
+var io_bufs: struct {
+    in: [4096]u8 = undefined,
+    out: [4096]u8 = undefined,
+} = .{};
 
 pub fn main() !void {
-    var stdout_writer = std.fs.File.stdout().writer(&stdout_buf);
+    const allocator = std.heap.smp_allocator;
+
+    var stdout_writer = std.fs.File.stdout().writer(&io_bufs.out);
     const stdout = &stdout_writer.interface;
 
-    const allocator = std.heap.smp_allocator;
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
 
-    if (args.len <= 1) {
-        std.process.fatal("no file provided", .{});
-    }
+    const file = try if (args.len <= 1) blk: {
+        break :blk std.fs.File.stdin();
+    } else blk: {
+        break :blk std.fs.cwd().openFile(args[1], .{});
+    };
 
-    const file_name = args[1];
-    const cwd = std.fs.cwd();
+    defer if (file.handle != std.posix.STDIN_FILENO) {
+        file.close();
+    };
 
-    var file = try cwd.openFile(file_name, .{});
-    defer file.close();
+    var file_reader = file.reader(&io_bufs.in);
 
-    var read_buf: [1024]u8 = undefined;
-    var file_reader = file.reader(&read_buf);
+    var content_writer_impl = std.Io.Writer.Allocating.init(allocator);
+    errdefer content_writer_impl.deinit();
+    const content_writer = &content_writer_impl.writer;
 
-    var content_writer = std.Io.Writer.Allocating.init(allocator);
-    defer content_writer.deinit();
+    _ = try content_writer.sendFile(&file_reader, .unlimited);
 
-    _ = try (&content_writer.writer).sendFile(&file_reader, .unlimited);
+    const content = try content_writer_impl.toOwnedSlice();
+    defer allocator.free(content);
 
-    const data = content_writer.writer.buffered();
-
-    var instrs = try ir.parse(allocator, data);
+    var instrs = try ir.parse(allocator, content);
     defer instrs.deinit(allocator);
 
     const code = try codegen.@"x86_64-linux".generate(allocator, instrs.items);
